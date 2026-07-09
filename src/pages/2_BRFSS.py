98950b9f86f3d1cd00f1b3e043690b56f4ffaad2
shannequin <shannequin@gmail.com>
2026-07-08 20:52:31 -0500

    Fix staging nhis pipeline for period, populate app nhis page

--- src/pages/2_BRFSS.py
+++ src/pages/2_BRFSS.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import streamlit as st
 
 from utils.custom_visuals import rainbow_divider
@@ -7,65 +8,68 @@ from utils.custom_visuals import rainbow_divider
 
 CONN = st.connection("postgresql", type="sql")
 
-def populate_sidebar() -> tuple['DataFrame', str]:
+def populate_sidebar() -> tuple[pd.DataFrame, str]:
     """
     Populate the sidebar with survey questions from the BRFSS dataset.
     """
+    selected_question = None
+    question_df = pd.DataFrame()
+
     st.sidebar.title('Survey Questions')
 
-    question_query = f"""SELECT *
-                        FROM staging.behavioral_risk_factor_surveillance_system_questions;"""
+    query = f"""SELECT *
+                FROM staging.behavioral_risk_factor_surveillance_system_questions;"""
 
-    brfss_question_df = CONN.query(sql=question_query)
+    question_df = CONN.query(sql=query)
 
-    brfss_question_df = brfss_question_df[brfss_question_df['Question'] != 'Interview Date']
+    question_df = question_df[question_df['Question'] != 'Interview Date']
 
     selected_question = st.sidebar.selectbox(
         label='Select a question:',
-        options=brfss_question_df['Question'].tolist(),
+        options=question_df['Question'].tolist(),
         index=None,
         placeholder='None'
     )
 
-    return brfss_question_df, selected_question
+    return question_df, selected_question
 
-def populate_body(brfss_question_df, selected_question) -> None:
+def populate_body(question_df: pd.DataFrame, question: str) -> None:
     """
     Populate the main body of the page with the selected survey question's results.
     """
-    if not selected_question:
+    if not question:
         st.info("Please select a question from the sidebar.")
 
     else:
-        column_name = brfss_question_df[brfss_question_df['Question'] == selected_question]['Field'].values[0]
+        column_name = question_df[question_df['Question'] == question]['Field'].values[0]
 
-        st.subheader(selected_question)
+        st.subheader(question, text_alignment='center')
 
-        results_query = f"""SELECT {column_name}, COUNT(*)
-                            FROM staging.behavioral_risk_factor_surveillance_system
-                            GROUP BY {column_name};"""
+        query = f"""SELECT {column_name}, COUNT(*)
+                    FROM staging.behavioral_risk_factor_surveillance_system
+                    GROUP BY {column_name};"""
 
-        brfss_response_df = CONN.query(sql=results_query)
+        survey_df = CONN.query(sql=query)
 
-        brfss_response_df = brfss_response_df.dropna(subset=[column_name, 'count'])
+        survey_df = survey_df.dropna(subset=[column_name, 'count'])
 
         fig, ax = plt.subplots()
         fig.set_size_inches(fig.get_size_inches()[0], fig.get_size_inches()[1] * 2)
 
-        colors = plt.cm.rainbow(np.linspace(1, 0, len(brfss_response_df)))
+        colors = plt.cm.rainbow(np.linspace(1, 0, len(survey_df)))
         ax.barh(
-            y=brfss_response_df[column_name].sort_values(ascending=False),
-            width=brfss_response_df.set_index(column_name).loc[brfss_response_df[column_name].sort_values(ascending=False)]['count'],
+            y=survey_df[column_name].sort_values(ascending=False),
+            width=survey_df.set_index(column_name).loc[survey_df[column_name].sort_values(ascending=False)]['count'],
             color=colors
         )
         ax.grid(axis='x', which='major', linestyle='--', alpha=0.7)
         ax.set_xlabel('Count')
-        ax.set_xlim(0, brfss_response_df['count'].mean() * 3)
+        ax.set_xlim(0, survey_df['count'].mean() * 3)
         ax.set_yticklabels([label.get_text()[:25] for label in ax.get_yticklabels()])
 
         st.pyplot(fig)
 
-        st.dataframe(brfss_response_df, hide_index=True)
+        st.dataframe(survey_df, hide_index=True)
 
 def main() -> None:
     """
@@ -76,9 +80,9 @@ def main() -> None:
 
     rainbow_divider()
 
-    brfss_question_df, selected_question = populate_sidebar()
+    question_df, question = populate_sidebar()
 
-    populate_body(brfss_question_df, selected_question)
+    populate_body(question_df, question)
 
 if __name__ == "__main__":
     main()
--- src/pages/3_NHIS.py
+++ src/pages/3_NHIS.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import re
 import streamlit as st
 
 from utils.custom_visuals import rainbow_divider
@@ -16,34 +18,60 @@ SURVEY_TABLE_MAP = {
     'Health Status': 'ohm08_respondent_assessed_health_status'
 }
 
-def populate_sidebar() -> str:
+def populate_sidebar() -> tuple[str, str]:
     """
     Populate the sidebar with survey options.
     """
+    selected_demographic = None
+    survey_df = pd.DataFrame()
 
     st.sidebar.title('Surveys')
 
+    survey_options = sorted(SURVEY_TABLE_MAP.keys())
     selected_survey = st.sidebar.selectbox(
         label='Select a survey:',
-        options=sorted(SURVEY_TABLE_MAP.keys()),
+        options=survey_options,
         index=None,
         placeholder='None'
     )
 
-    return selected_survey
+    if selected_survey:
+        table_name = SURVEY_TABLE_MAP.get(selected_survey)
 
-def populate_body(selected_survey) -> None:
+        query = f"""SELECT *
+                    FROM staging.{table_name};"""
+
+        survey_df = CONN.query(sql=query)
+
+        survey_title = re.sub(r' - OHM.+', '', survey_df['title'].iloc[0])
+        st.subheader(survey_title, text_alignment='center')
+
+        demographic_options = [re.sub(r' \* \^.+', '', demographic) for demographic in survey_df['demographic_group'].unique()]
+
+        selected_demographic = st.sidebar.selectbox(
+            label='Select a demographic:',
+            options=sorted(demographic_options),
+            index=None,
+            placeholder='None'
+        )
+
+    return selected_survey, selected_demographic, survey_df
+
+def populate_body(survey: str, demographic: str, survey_df: pd.DataFrame) -> None:
     """
     Populate the body with the selected survey's data.
     """
-    if not selected_survey:
+    if not survey:
         st.info("Please select a survey from the sidebar.")
+
+    elif not demographic:
+        st.info("Please select a demographic from the sidebar.")
+
     else:
-        st.subheader(selected_survey)
+        survey_df = survey_df.loc[survey_df['demographic_group'].str.contains(demographic, regex=False)]
+        survey_df = survey_df.drop(columns=['code', 'title', 'demographic_group']).sort_values('population')
+        st.dataframe(survey_df, hide_index=True)
 
-        table_name = SURVEY_TABLE_MAP.get(selected_survey) #TODO: Change to table title field
-
-        st.write(f"Displaying data from table: {table_name}")
 
 def main() -> None:
     """
@@ -53,9 +81,9 @@ def main() -> None:
 
     rainbow_divider()
 
-    selected_survey = populate_sidebar()
+    survey, demographic, survey_df = populate_sidebar()
 
-    populate_body(selected_survey)
+    populate_body(survey, demographic, survey_df)
 
 if __name__ == "__main__":
     main()
